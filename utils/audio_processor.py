1c97191021cd0958d73e9958cddaf011c320573d
b-atmaja <b-atmaja@aist.go.jp>
2022-01-22 09:16:54 +0900

    add normalization

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -1,4 +1,8 @@
 import torchaudio
+# import s3prl.hub as hub
+from sklearn.preprocessing import StandardScaler
+
+SCALER = StandardScaler()
 
 class AudioProcessor(object):
     def __init__(self, feature, num_mels, num_mfcc, log_mels, mel_fmin, mel_fmax, normalize, sample_rate, n_fft, num_freq, hop_length, win_length):
@@ -26,18 +30,23 @@ class AudioProcessor(object):
             audio_class = torchaudio.transforms.MelSpectrogram(sample_rate=self.sample_rate, n_fft=self.n_fft, win_length=self.win_length, hop_length=self.hop_length, n_mels=self.num_mels, f_min=self.mel_fmin, f_max=self.mel_fmax)
         elif self.feature == 'mfcc':
             audio_class = torchaudio.transforms.MFCC(sample_rate=self.sample_rate, n_mfcc=self.num_mfcc, log_mels=self.log_mels, melkwargs={'n_fft':self.n_fft, 'win_length':self.win_length, 'hop_length':self.hop_length, 'n_mels':self.num_mels})
-
+        # elif self.feature == 'wav2vec2':
+        #     audio_class = getattr(hub, 'wav2vec2')()
+            
         feature = audio_class(y)
         return feature
 
     def get_feature_from_audio_path(self, audio_path):
-        return self.wav2feature(self.load_wav(audio_path))
+        feat = self.wav2feature(self.load_wav(audio_path))
+        return SCALER.fit_transform(feat)
 
     def get_feature_from_audio(self, wav):
-        return self.wav2feature(wav)
+        feat = self.wav2feature(wav)
+        return SCALER.fit_transform(feat)
 
     def load_wav(self, path):
-        wav, sample_rate = torchaudio.load(path, normalization=self.normalize)
+        # load wav and normalize to [-1, 1]
+        wav, sample_rate = torchaudio.load(path, normalize=self.normalize)
         # resample audio for specific samplerate
         if sample_rate != self.sample_rate:
             resample = torchaudio.transforms.Resample(sample_rate, self.sample_rate)
